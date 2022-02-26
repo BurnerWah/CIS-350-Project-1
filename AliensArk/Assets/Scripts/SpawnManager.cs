b9131c33a886c2a941b1bd168aac981bea6725ab
Jaden <jpleasants@mail.bradley.edu>
2022-02-25 20:28:18 -0600

    Spawn manager now places aliens on slots correctly

--- AliensArk/Assets/Scripts/SpawnManager.cs
+++ AliensArk/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@
  */
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
@@ -16,6 +17,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject ailenPrefab;
 
     private TurnManager TM;
+    private SlotManager SM;
 
     // This counter should automatically be reset as it's updeated
     private int _turnsSinceLastSpawn;
@@ -33,6 +35,7 @@ public class SpawnManager : MonoBehaviour
         turnsSinceLastSpawn = 0;
         TM = TurnManager.GetTurnManager();
         TM.TurnEvent.AddListener(SpawnAlien);
+        SM = GameObject.Find("/SlotManager").GetComponent<SlotManager>();
     }
 
     void OnDestroy()
@@ -42,9 +45,25 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnAlien()
     {
-        if (TM.currentTurn > turnsBeforeFirstSpawn && turnsSinceLastSpawn++ == 0)
+        // This is using LINQ, which is more or less SQL for C#
+        // I don't know why it's a thing but eh it works.
+        var viableSlots =
+            from slot in SM.PlanetSlots
+            let slotC = slot.GetComponent<Slot>()
+            where slotC.alien == null && !slotC.IsHidden
+            orderby Random.Range(0f, 1f) descending // This just sorts randomly
+            select slot;
+        if (TM.currentTurn > turnsBeforeFirstSpawn
+            && turnsSinceLastSpawn++ == 0
+            && viableSlots.Count() > 0)
         {
-            Instantiate(ailenPrefab);
+            var slot = viableSlots.First();
+            var position = new Vector3(slot.transform.position.x, slot.transform.position.y, -1);
+            var alien = Instantiate(ailenPrefab, position, ailenPrefab.transform.rotation);
+            var slotC = slot.GetComponent<Slot>();
+            var alienC = alien.GetComponent<Alien>();
+            slotC.alien = alienC;
+            alienC.slot = slotC;
         }
     }
 }
